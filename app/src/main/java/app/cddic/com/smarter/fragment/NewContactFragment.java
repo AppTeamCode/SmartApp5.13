92f1439fa6d7c08f1061c55db61584daf71d75cc
Hai <1455166220@qq.com>
2017-05-15 14:38:58 +0800

    wwh

---
+++ app/src/main/java/app/cddic/com/smarter/fragment/NewContactFragment.java
@@ -0,0 +1,8 @@
+package app.cddic.com.smarter.fragment;
+
+/**
+ * Created by Hai on 2017/5/10.
+ */
+
+public class NewContactFragment {
+}
---
+++ app/src/main/java/app/cddic/com/smarter/widget/DeviceDetailsGridView.java
@@ -0,0 +1,8 @@
+package app.cddic.com.smarter.widget;
+
+/**
+ * Created by Hai on 2017/5/10.
+ */
+
+public class DeviceDetailsGridView {
+}
